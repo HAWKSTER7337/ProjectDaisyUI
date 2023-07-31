d66aa56ee27fca8f4dbc237b3537f7ce9aec09b5
HAWKSTER7337 <109770480+HAWKSTER7337@users.noreply.github.com>
2023-07-30 23:56:45 -0400

    Implemented Requests to website code

--- Daily3-UI/MainPage.xaml.cs
+++ Daily3-UI/MainPage.xaml.cs
@@ -4,42 +4,48 @@ namespace Daily3_UI;
 
 public partial class MainPage : ContentPage
 {
-	public WinningNumber YesterdayMidday { get; } = new WinningNumber(1,8,5);
-    public WinningNumber YesterdayEvening { get; } = new WinningNumber(8,2,5);
-    public WinningNumber TodayMidday { get; } = new WinningNumber(8, 2, 7);
-    public WinningNumber TodayEvening { get; } = new WinningNumber(7, 2, 6);
+	public WinningNumber YesterdayMidday { get; private set; }
+    public WinningNumber YesterdayEvening { get; private set; }
+    public WinningNumber TodayMidday { get; private set; }
+    public WinningNumber TodayEvening { get; private set; }
 
     public MainPage()
 	{
-		InitializeComponent();
+        InitializeComponent();
         UpdatePage();
     }
 
     /// <summary>
     /// Updates all of the values to what they are stored as
     /// </summary>
-    async private void UpdatePage()
+    private async void UpdatePage()
     {
-        Task InnerFunction()
+        List<WinningNumber> winningNumbers = await WebScraper.GetWinningNumbers();
+
+        YesterdayMidday = winningNumbers[0];
+        yesterdayMidday1.Text = YesterdayMidday.Number1.ToString();
+        yesterdayMidday2.Text = YesterdayMidday.Number2.ToString();
+        yesterdayMidday3.Text = YesterdayMidday.Number3.ToString();
+
+        YesterdayEvening = winningNumbers[1];
+        yesterdayEvening1.Text = YesterdayEvening.Number1.ToString();
+        yesterdayEvening2.Text = YesterdayEvening.Number2.ToString();
+        yesterdayEvening3.Text = YesterdayEvening.Number3.ToString();
+
+        if (winningNumbers.Count > 2)
         {
-            yesterdayMidday1.Text = YesterdayMidday.Number1.ToString();
-            yesterdayMidday2.Text = YesterdayMidday.Number2.ToString();
-            yesterdayMidday3.Text = YesterdayMidday.Number3.ToString();
-
-            yesterdayEvening1.Text = YesterdayEvening.Number1.ToString();
-            yesterdayEvening2.Text = YesterdayEvening.Number2.ToString();
-            yesterdayEvening3.Text = YesterdayEvening.Number3.ToString();
-
+            TodayMidday = winningNumbers[2];
             todayMidday1.Text = TodayMidday.Number1.ToString();
             todayMidday2.Text = TodayMidday.Number2.ToString();
             todayMidday3.Text = TodayMidday.Number3.ToString();
-
+        }
+        if (winningNumbers.Count > 3)
+        {
+            TodayEvening = winningNumbers[3];
             todayEvening1.Text = TodayEvening.Number1.ToString();
             todayEvening2.Text = TodayEvening.Number2.ToString();
             todayEvening3.Text = TodayEvening.Number3.ToString();
-            return Task.CompletedTask;
         }
-        await InnerFunction();
     }
 }
 
