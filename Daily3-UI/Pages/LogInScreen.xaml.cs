3b21baea94052fac5cbae354f2ac84a1290d75b7
HAWKSTER7337 <109770480+HAWKSTER7337@users.noreply.github.com>
2023-08-08 11:53:23 -0400

    refactor of namespaces

---
+++ Daily3-UI/Pages/LogInScreen.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Daily3_UI.Pages;
+
+/// <summary>
+///     Page for handling logging into the application
+/// </summary>
+public partial class LogInScreen : ContentPage
+{
+    public LogInScreen()
+    {
+        InitializeComponent();
+    }
+}
