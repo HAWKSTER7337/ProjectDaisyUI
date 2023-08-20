5a9b94a1d237b3ec2395918a29d67b1ff474172d
HAWKSTER7337 <109770480+HAWKSTER7337@users.noreply.github.com>
2023-08-19 22:56:40 -0400

    Cleaned request

--- Daily3-UI/Clients/BuyTicketClient.cs
+++ Daily3-UI/Clients/BuyTicketClient.cs
@@ -1,36 +1,26 @@
-﻿using System.Web;
+﻿using Daily3_UI.Classes;
 using Daily3_UI.Enums;
 
 namespace Daily3_UI.Clients;
 
 public static class BuyTicketClient
 {
-    public static async Task BuyTicket
-    (
-        int number1,
-        int number2,
-        int number3,
-        string price,
-        TicketType ticketType,
-        TOD timeOfDay,
-        string date
-    )
+    /// <summary>
+    ///     Client For Buying a Ticket
+    /// </summary>
+    public static async Task<string> BuyTicket(Ticket ticket)
     {
         var httpClient = new HttpClient();
         var baseUrl = "http://10.0.2.2:5198/";
         var endPoint = "api/BuyTicket";
 
-        var uriBuilder = new UriBuilder(baseUrl + endPoint);
-        var queryParameters = HttpUtility.ParseQueryString(string.Empty);
-        queryParameters["number1"] = number1.ToString();
-        queryParameters["number2"] = number2.ToString();
-        queryParameters["number3"] = number3.ToString();
-        queryParameters["price"] = price;
-        queryParameters["ticketType"] = ((int)ticketType).ToString();
-        queryParameters["timeOfDay"] = ((int)timeOfDay).ToString();
-        queryParameters["date"] = date;
-        uriBuilder.Query = queryParameters.ToString();
-        var apiUrl = uriBuilder.ToString();
+        var apiUrl = ticket.ToApiUrl(baseUrl, endPoint);
+
+        if (!isValidDate(ticket.TimeOfDay, ticket.Date))
+        {
+            Console.WriteLine("Not valid date to buy Ticket");
+            return "Not Valid Date to Buy Ticket";
+        }
 
         try
         {
@@ -40,14 +30,27 @@ public static class BuyTicketClient
             {
                 var responseBody = await response.Content.ReadAsStringAsync();
                 Console.WriteLine("Response: " + responseBody);
-                return;
+                return "";
             }
 
             Console.WriteLine("Request failed with status code: " + response.StatusCode);
+            return "Request failed";
         }
         catch (Exception ex)
         {
             Console.WriteLine("Error: " + ex.Message);
+            return "Shi....ooot something went wrong!";
         }
+    }
+
+    /// <summary>
+    ///     Validating that the date given is still valid to buy Tickets for
+    /// </summary>
+    /// <param name="timeOfDay">The Time of day that the ticket is being bought for</param>
+    /// <param name="date">The date that the ticket is designed for</param>
+    private static bool isValidDate(TOD? timeOfDay, string date)
+    {
+        // todo make this work and actually pick a real time
+        return true;
     }
 }
