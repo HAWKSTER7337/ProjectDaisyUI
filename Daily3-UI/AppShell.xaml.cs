9a63e4cdf20a82e325986029149fb77ca756ced6
HAWKSTER7337 <109770480+HAWKSTER7337@users.noreply.github.com>
2024-08-26 18:38:11 -0400

    Add admin-only "Entrants Tickets" tab
    Implemented a new "Entrants Tickets" tab that appears only for users with admin status. Updated AppShell.xaml.cs to check user status during the OnAppearing event and dynamically add the tab if the user is an admin.

--- Daily3-UI/AppShell.xaml.cs
+++ Daily3-UI/AppShell.xaml.cs
@@ -1,9 +1,32 @@
-﻿namespace Daily3_UI;
+﻿using Daily3_UI.Classes;
+using Daily3_UI.Enums;
+using Daily3_UI.Pages;
+
+namespace Daily3_UI;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var isAdmin =
+            !(Globals.Status is null) && // Checking if the value is defined
+            Globals.Status >= Status.House; // Checking if they are the proper Status
+        if (!isAdmin) return;
+
+        var shellContent = new ShellContent
+        {
+            Title = "Entrants Tickets",
+            ContentTemplate = new DataTemplate(typeof(TicketHistory)),
+            Route = "TicketHistory"
+        };
+
+        TabBar.Items.Add(shellContent);
     }
 }
