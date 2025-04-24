2130d90a07703073170a410d17e14d33623dcd1b
HAWKSTER7337 <ahawk2255@gmail.com>
2025-04-24 16:02:03 -0400

    Added a new font for apple

--- Daily3-UI/Platforms/iOS/AppDelegate.cs
+++ Daily3-UI/Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,6 @@
-﻿using Foundation;
+﻿using System.Diagnostics;
+using Foundation;
+using UIKit;
 
 namespace Daily3_UI;
 
@@ -8,5 +10,31 @@ public class AppDelegate : MauiUIApplicationDelegate
     protected override MauiApp CreateMauiApp()
     {
         return MauiProgram.CreateMauiApp();
+    }
+
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    {
+        // Set the font name - this must be the internal font name, not the filename
+        var fontName = "BebasNeue-Regular"; // Replace with your actual font name
+        var fontSize = 14f;
+
+        var customFont = UIFont.FromName(fontName, fontSize);
+
+        if (customFont != null)
+        {
+            var attributes = new UIStringAttributes
+            {
+                Font = customFont
+            };
+
+            UITabBarItem.Appearance.SetTitleTextAttributes(attributes, UIControlState.Normal);
+            UITabBarItem.Appearance.SetTitleTextAttributes(attributes, UIControlState.Selected);
+        }
+        else
+        {
+            Debug.WriteLine($"❌ Font '{fontName}' could not be loaded.");
+        }
+
+        return base.FinishedLaunching(app, options);
     }
 }
