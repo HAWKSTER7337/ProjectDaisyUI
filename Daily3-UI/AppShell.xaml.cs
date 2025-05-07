194b3672ef7d9a7553d25327f5503a9ec10eb406
HAWKSTER7337 <ahawk2255@gmail.com>
2025-05-06 20:30:13 -0400

    Removed unused code

--- Daily3-UI/AppShell.xaml.cs
+++ Daily3-UI/AppShell.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Daily3_UI.Classes;
-using Daily3_UI.Enums;
-using Daily3_UI.Pages;
+﻿using Daily3_UI.Pages;
 using Daily3_UI.Pages.PagesDaily3;
 using TicketHistory = Daily3_UI.Pages.TicketHistory;
 
@@ -11,16 +9,15 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
-        
+
         // Register routes
         Routing.RegisterRoute("WinningNumbers3", typeof(WinningNumbersPage));
         Routing.RegisterRoute("Entrants3", typeof(HousePage));
-        
+
         Routing.RegisterRoute("WinningNumbers4", typeof(WinningNumbersPageDaily4));
-        Routing.RegisterRoute("Entrants4", typeof(HousePageDaily4));
-        
+
         Routing.RegisterRoute("HistoryPage", typeof(TicketHistory));
         Routing.RegisterRoute("ChangePassword", typeof(ChangePasswordPage));
         Routing.RegisterRoute("CreateAccount", typeof(CreateAccountPage));
     }
-} 
+}
