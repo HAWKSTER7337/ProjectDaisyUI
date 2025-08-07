2957f6aec4d4c624a19659bf64d2312dfb9899d0
HAWKSTER7337 <ahawk2255@gmail.com>
2025-08-06 22:50:18 -0400

    Houses can now add users under them

---
+++ Daily3-UI/CustomPopUps/TicketAdminConfirmNewUserPopUp.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace Daily3_UI.CustomPopUps;
+
+public class TicketAdminConfirmNewUserPopUp : Popup
+{
+    public TicketAdminConfirmNewUserPopUp(string username)
+    {
+        var label = new Label
+        {
+            Text = $"Are you sure you want to add {username} under you?",
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+            Margin = new Thickness(10)
+        };
+
+        var yesButton = new Button
+        {
+            Text = "Yes",
+            HorizontalOptions = LayoutOptions.EndAndExpand
+        };
+
+        var noButton = new Button
+        {
+            Text = "No",
+            HorizontalOptions = LayoutOptions.StartAndExpand
+        };
+
+        yesButton.Clicked += (s, e) => { Close(true); };
+
+        noButton.Clicked += (s, e) => { Close(false); };
+
+        var buttonLayout = new StackLayout
+        {
+            Orientation = StackOrientation.Horizontal,
+            HorizontalOptions = LayoutOptions.Center,
+            Children = { noButton, yesButton }
+        };
+
+        Content = new VerticalStackLayout
+        {
+            Spacing = 20,
+            Padding = new Thickness(20),
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+            Children = { label, buttonLayout }
+        };
+    }
+}
--- Daily3-UI/Pages/HouseAddUsersPage.xaml.cs
+++ Daily3-UI/Pages/HouseAddUsersPage.xaml.cs
@@ -1,20 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Maui.Views;
+using Daily3_UI.Classes;
+using Daily3_UI.Clients;
+using Daily3_UI.CustomPopUps;
 
 namespace Daily3_UI.Pages;
 
 public partial class HouseAddUsersPage : ContentPage
 {
+    private List<User> _users = new();
+
+    public List<User> Users()
+    {
+        return _users.OrderBy(user => user.Username).ToList();
+    }
+
     public HouseAddUsersPage()
     {
         InitializeComponent();
     }
-    
+
+    protected override async void OnAppearing()
+    {
+        await LoadScreen();
+    }
+
+    private async Task LoadScreen()
+    {
+        try
+        {
+            UserLoader.IsRunning = true;
+            UserLoader.IsVisible = true;
+
+            _users = await GetUsersAndTicketsUnderHouse.GetUnverifiedUsers();
+
+            UserCollectionView.ItemsSource = Users();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+        finally
+        {
+            UserLoader.IsRunning = false;
+            UserLoader.IsVisible = false;
+        }
+    }
+
     private async void OnHousePageButtonClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("..");
+    }
+
+    private async void RequestToAddUser(object sender, EventArgs e)
+    {
+        if (sender is not Button button) return;
+        var user = button.BindingContext as User;
+
+        var isAddingUserUnderThem =
+            await Application.Current.MainPage.ShowPopupAsync(new TicketAdminConfirmNewUserPopUp(user.Username)) as bool
+                ?;
+
+        if (isAddingUserUnderThem != true) return;
+        await GetUsersAndTicketsUnderHouse.AddUserUnderHouse(user.UserId);
+        await LoadScreen();
     }
 }
