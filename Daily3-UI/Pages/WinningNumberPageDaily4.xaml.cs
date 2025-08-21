07aaaeb193f334132092150821be905e0810ee95
Alex Hawkins <hawkins@Alexs-Mac-mini.local>
2025-08-20 23:34:17 -0400

    Added more reponsiveness to the winning numbers page

--- Daily3-UI/Pages/WinningNumberPageDaily4.xaml.cs
+++ Daily3-UI/Pages/WinningNumberPageDaily4.xaml.cs
@@ -15,6 +15,52 @@ public partial class WinningNumbersPageDaily4 : ContentPage
     private WinningNumberDaily4 TodayMidday { get; set; }
     private WinningNumberDaily4 TodayEvening { get; set; }
 
+    private bool _isLoading = true;
+
+    public bool IsLoading
+    {
+        get => _isLoading;
+        private set
+        {
+            _isLoading = value;
+
+            if (_isLoading)
+            {
+                LoadingIndicator.IsVisible = true;
+                ErrorContainer.IsVisible = false;
+                TodayLabel.IsVisible = false;
+                TodayMiddayBorder.IsVisible = false;
+                TodayEveningBorder.IsVisible = false;
+                YesterdayLabel.IsVisible = false;
+                YesterdayMiddayBorder.IsVisible = false;
+                YesterdayEveningBorder.IsVisible = false;
+            }
+            else
+            {
+                LoadingIndicator.IsVisible = false;
+                ErrorContainer.IsVisible = false;
+                TodayLabel.IsVisible = true;
+                TodayMiddayBorder.IsVisible = true;
+                TodayEveningBorder.IsVisible = true;
+                YesterdayLabel.IsVisible = true;
+                YesterdayMiddayBorder.IsVisible = true;
+                YesterdayEveningBorder.IsVisible = true;
+            }
+        }
+    }
+
+    private void ShowError()
+    {
+        LoadingIndicator.IsVisible = false;
+        ErrorContainer.IsVisible = true;
+        TodayLabel.IsVisible = false;
+        TodayMiddayBorder.IsVisible = false;
+        TodayEveningBorder.IsVisible = false;
+        YesterdayLabel.IsVisible = false;
+        YesterdayMiddayBorder.IsVisible = false;
+        YesterdayEveningBorder.IsVisible = false;
+    }
+
     protected override async void OnAppearing()
     {
         await UpdatePage();
@@ -30,45 +76,69 @@ public partial class WinningNumbersPageDaily4 : ContentPage
     /// </summary>
     private async Task UpdatePage()
     {
+        IsLoading = true;
         ClearWinningNumbers();
-        var winningNumbers = await WinningNumbersClient.GetWinningNumbersDaily4();
-
-        switch (winningNumbers.Count)
+        
+        try
         {
-            case 4:
-                TodayEvening = winningNumbers[3];
-                TodayEvening1.Text = TodayEvening.Number1.ToString();
-                TodayEvening2.Text = TodayEvening.Number2.ToString();
-                TodayEvening3.Text = TodayEvening.Number3.ToString();
-                TodayEvening4.Text = TodayEvening.Number4.ToString();
-                goto case 3;
-            case 3:
-                TodayMidday = winningNumbers[2];
-                TodayMidday1.Text = TodayMidday.Number1.ToString();
-                TodayMidday2.Text = TodayMidday.Number2.ToString();
-                TodayMidday3.Text = TodayMidday.Number3.ToString();
-                TodayMidday4.Text = TodayMidday.Number4.ToString();
-                goto case 2;
-            case 2:
-                YesterdayEvening = winningNumbers[1];
-                YesterdayEvening1.Text = YesterdayEvening.Number1.ToString();
-                YesterdayEvening2.Text = YesterdayEvening.Number2.ToString();
-                YesterdayEvening3.Text = YesterdayEvening.Number3.ToString();
-                YesterdayEvening4.Text = YesterdayEvening.Number4.ToString();
-                goto case 1;
-            case 1:
-                YesterdayMidday = winningNumbers[0];
-                YesterdayMidday1.Text = YesterdayMidday.Number1.ToString();
-                YesterdayMidday2.Text = YesterdayMidday.Number2.ToString();
-                YesterdayMidday3.Text = YesterdayMidday.Number3.ToString();
-                YesterdayMidday4.Text = YesterdayMidday.Number4.ToString();
-                break;
+            var winningNumbers = await WinningNumbersClient.GetWinningNumbersDaily4();
+
+            if (winningNumbers == null || winningNumbers.Count == 0)
+            {
+                ShowError();
+                return;
+            }
+
+            switch (winningNumbers.Count)
+            {
+                case 4:
+                    TodayEvening = winningNumbers[3];
+                    TodayEvening1.Text = TodayEvening.Number1.ToString();
+                    TodayEvening2.Text = TodayEvening.Number2.ToString();
+                    TodayEvening3.Text = TodayEvening.Number3.ToString();
+                    TodayEvening4.Text = TodayEvening.Number4.ToString();
+                    goto case 3;
+                case 3:
+                    TodayMidday = winningNumbers[2];
+                    TodayMidday1.Text = TodayMidday.Number1.ToString();
+                    TodayMidday2.Text = TodayMidday.Number2.ToString();
+                    TodayMidday3.Text = TodayMidday.Number3.ToString();
+                    TodayMidday4.Text = TodayMidday.Number4.ToString();
+                    goto case 2;
+                case 2:
+                    YesterdayEvening = winningNumbers[1];
+                    YesterdayEvening1.Text = YesterdayEvening.Number1.ToString();
+                    YesterdayEvening2.Text = YesterdayEvening.Number2.ToString();
+                    YesterdayEvening3.Text = YesterdayEvening.Number3.ToString();
+                    YesterdayEvening4.Text = YesterdayEvening.Number4.ToString();
+                    goto case 1;
+                case 1:
+                    YesterdayMidday = winningNumbers[0];
+                    YesterdayMidday1.Text = YesterdayMidday.Number1.ToString();
+                    YesterdayMidday2.Text = YesterdayMidday.Number2.ToString();
+                    YesterdayMidday3.Text = YesterdayMidday.Number3.ToString();
+                    YesterdayMidday4.Text = YesterdayMidday.Number4.ToString();
+                    break;
+            }
+
+            IsLoading = false;
+        }
+        catch (Exception ex)
+        {
+            // Log the exception if you have logging configured
+            System.Diagnostics.Debug.WriteLine($"Error loading winning numbers: {ex.Message}");
+            ShowError();
         }
     }
 
     private async void ToBuyTicketPage(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("..");
+    }
+
+    private async void RetryButton_Clicked(object sender, EventArgs e)
+    {
+        await UpdatePage();
     }
 
     private void ClearWinningNumbers()
